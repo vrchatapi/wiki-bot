3aa137e7c1864b526a025fc4274e31863f504996
Aries <10256477+ariesclark@users.noreply.github.com>
2024-08-20 19:09:54 -0600

    feat: add support for infopush content images & extra links

--- src/templates/main-page/infopush.tsx
+++ src/templates/main-page/infopush.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable sort-keys/sort-keys-fix */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { renderToStaticMarkup } from "react-dom/server";
 
 import { normalizeVRChat } from "~/api/vrchat";
@@ -19,16 +21,38 @@ type SupportedCommand = (typeof supportedCommands)[number];
 function getArticle({ name, article, imageUrl }: InfoPushData) {
 	if (!article) return null;
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const { content, embeddedLinkData, moreInfoLinks } = article as any;
+	const { content, embeddedLinkData, moreInfoLinks } = article as {
+		content: Array<any>;
+		embeddedLinkData: Array<any>;
+		moreInfoLinks: Array<any>;
+	};
 	if (!Array.isArray(content)) return null;
 
+	const links = moreInfoLinks
+		.filter(({ command }) => command === "OpenURL")
+		.map(({ name, parameters: [url] }) => ({ name, url }));
+
 	return {
+		title: name,
+		image: imageUrl,
 		content: renderToStaticMarkup(
 			<>
+				{imageUrl && (
+					<img
+						src={imageUrl}
+						style={{
+							borderRadius: "8px",
+							display: "block",
+							float: "right",
+							marginLeft: "1em",
+							width: "32%"
+						}}
+					></img>
+				)}
 				{content.map((value, index) => {
 					if ("text" in value) {
 						const tree = richtext.toSyntaxTree(value.text);
+
 						return (
 							<richtext.Element
 								node={tree}
@@ -61,11 +85,43 @@ function getArticle({ name, article, imageUrl }: InfoPushData) {
 							/>
 						);
 					}
+
+					if ("imageUrl" in value)
+						return (
+							<img
+								src={value.imageUrl}
+								key={index}
+								style={{
+									borderRadius: "8px",
+									margin: "1em 0",
+									width: "100%"
+								}}
+							/>
+						);
 				})}
+				{links.length > 0 && (
+					<div style={{ marginTop: "2em" }}>
+						{links.map(({ name, url }, index) => (
+							<a
+								key={index}
+								href={url}
+								title={name}
+								style={{
+									display: "flex",
+									padding: "1em 2em",
+									backgroundColor: "var(--less-dark-grey)",
+									color: "white",
+									width: "fit-content",
+									borderRadius: "8px"
+								}}
+							>
+								{name}
+							</a>
+						))}
+					</div>
+				)}
 			</>
-		),
-		image: imageUrl,
-		title: name
+		)
 	};
 }
 
@@ -106,12 +162,11 @@ export async function refresh() {
 							if (!article) return {};
 
 							return {
+								"article-id": articleId,
+								"article-title": article.title,
 								"article-content": Buffer.from(
 									normalizeVRChat(article.content)
-								).toString("base64"),
-								"article-id": articleId,
-								"article-image": article.image,
-								"article-title": article.title
+								).toString("base64")
 							};
 						})(),
 						OpenSafetyMenu: {
